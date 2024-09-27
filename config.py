8128e21e847ccf103220507df3fecd56cc46ca55
Vigo <miroslav.viktorin@seznam.cz>
2024-09-27 22:46:20 +0200

    makame

--- config.py
+++ config.py
@@ -1,16 +1,14 @@
 # region Importy
-import logging
-from tkinter import font
-from tkinter.tix import COLUMN
+import logging as lg
 import typing
 from pathlib import Path
-
+from tkinter import font
 from customtkinter import filedialog
 from PIL import Image
 
 # endregion
 
-logging.basicConfig(level=logging.INFO)
+lg.basicConfig(level=lg.INFO)
 
 # region Promenne
 border_radius: int = 4
@@ -20,13 +18,18 @@ medium_font: tuple = ("Helvetica", 18)
 large_font: tuple = ("Helvetica", 22)
 good_color: str = "#218909"
 error_color: str = "#a75a02"
+layout_config: dict = {
+    "border_width": 1,
+    "border_color": "gray",
+    "corner_radius": 4,
+}
 
 
 def img_loader(ctk, vstup_dir: str, size_img: int):
     """Nahrává obrázky do apky."""
     img = ctk.CTkImage(light_image=Image.open(vstup_dir), size=(size_img, size_img))
     if not img:
-        logging.error(f"Nepodařilo se načíst obrázek z {vstup_dir}")
+        lg.error(f"Nepodařilo se načíst obrázek z {vstup_dir}")
         return None
     return img
 
@@ -43,7 +46,8 @@ def appearance_mode(theme: str, color: str, ctk) -> None:
     #
     ctk.set_widget_scaling(1.0)
     ctk.set_window_scaling(1.0)
-    logging.info("Theme: %s, barva: %s", theme, color)
+    lg.info("Theme: %s, barva: %s", theme, color)
+    # konec funkce
 
 
 def app_init(self, name, width, height) -> None:
@@ -53,6 +57,7 @@ def app_init(self, name, width, height) -> None:
     center_window(self, width, height)
     self.minsize(width, height)
     self.resizable(True, True)
+    # konec funkce
 
 
 def center_window(self, app_width: int, app_height: int) -> None:
@@ -65,20 +70,21 @@ def center_window(self, app_width: int, app_height: int) -> None:
     x: int = screen_width // 2 - width // 2
     y: int = screen_height // 2 - height // 2
     self.geometry(f"{width}x{height}+{x}+{y}")
-    logging.info("Rozliseni: %s x %s.", width, height)
+    lg.info("Rozliseni: %s x %s.", width, height)
+    # konec funkce
 
 
 # endregion
 #
 # region MENU
-def create_menu(self, mb, cdm) -> tuple:
+def create_menu(self, mb, cdm, ctk) -> tuple:
+    """Vytvor menu bar"""
     menu = mb(self, height=40, padx=10)
     buttons: dict = {
         "system_btn": "System",
         "settings_btn": "Settings",
         "info_btn": "Info",
     }
-
     config: dict = {"corner_radius": border_radius, "font": menu_font}
     column: int = len(buttons)
 
@@ -89,7 +95,10 @@ def create_menu(self, mb, cdm) -> tuple:
 
     # drop menu
     system_drop = cdm(widget=self.system_btn, **config)
-    self.system_settings = system_drop.add_option("Settings")
+    self.system_settings = system_drop.add_option(
+        "Settings", command=lambda: appearance(self, ctk)
+    )
+
     system_drop.add_separator()
     self.konec = system_drop.add_option("Exit", command=self.destroy)
     # --
@@ -101,57 +110,78 @@ def create_menu(self, mb, cdm) -> tuple:
     # --
     # end drop menu -------------------------
     menu.columnconfigure(column, weight=1)
+
     return (menu, column)
-
-
-# endregion
-#
-# region DETECT_DB
-def detect_db(self, ctk, parent, column: int) -> None:
-    error_img = img_loader(ctk, "./assets/exclamation2.png", 40)
-    #
-    detect_db_frame = ctk.CTkFrame(parent, fg_color=parent._fg_color)
-    detect_db_frame.grid(row=0, column=column, sticky="e", padx=20)
-    #
-    detect_db_error = ctk.CTkLabel(
-        detect_db_frame,
-        text="",
-        image=error_img,
-        compound="center",
-    )
-    detect_db_error.grid(row=0, column=0, sticky="ew")
-    #
-    detect_db_label = ctk.CTkLabel(
-        detect_db_frame, text="", font=medium_font, corner_radius=5
-    )
-    detect_db_label.grid(row=0, column=1, sticky="ew", ipady=5, ipadx=5, padx=5)
-    #
-    match self.mongo_db:
-        case True:
-            detect_db_error.grid_remove()
-            detect_db_label.configure(text="MongoDB: connected", fg_color=good_color)
-            logging.info("MongoDB: connected")
-
-        case False:
-            detect_db_error.grid()
-            detect_db_label.configure(
-                text="MongoDB: disconnected",
-                fg_color=error_color,
-            )
-
-            logging.error("MongoDB: disconnected!")
+    # konec funkce
 
 
 # endregion
 #
 # region FUNKCE
+#
+# region DETECT_DB
+def db_frame(self, ctk, parent, column: int) -> None:
+    """vytvor kontrolni frame pro databazi"""
+    error_img = img_loader(ctk, "./assets/exclamation2.png", 40)
+    #
+    self.detect_db_frame = ctk.CTkFrame(parent, fg_color=parent._fg_color)
+    self.detect_db_frame.grid(row=0, column=column, sticky="e", padx=20)
+    #
+    self.detect_db_error = ctk.CTkLabel(
+        self.detect_db_frame,
+        text="",
+        image=error_img,
+        compound="center",
+    )
+    self.detect_db_error.grid(row=0, column=0, sticky="ew")
+    #
+    self.detect_db_label = ctk.CTkLabel(
+        self.detect_db_frame, text="", font=medium_font, corner_radius=5
+    )
+    self.detect_db_label.grid(row=0, column=1, sticky="ew", ipady=5, ipadx=5, padx=5)
+    # konec funkce
 
 
-def test(self) -> None:
-    self.flag = True
+def detect_db(self, mongo):
+    """detekuj databazi"""
+    match mongo:
+        case True:
+            self.detect_db_error.grid_remove()
+            self.detect_db_label.configure(
+                text="MongoDB: connected", fg_color=good_color
+            )
+            log("MongoDB: connect", "Info", mongo)
+
+        case False:
+            self.detect_db_error.grid()
+            self.detect_db_label.configure(
+                text="MongoDB: disconnected",
+                fg_color=error_color,
+            )
+            log("MongoDB: connect", "Error", mongo)
+    # konec funkce
+
+
+# endregion
+
+
+# appearance
+def appearance(self, ctk):
+    "zmena theme"
+    if self.theme == "dark":
+        self.theme = "light"
+        ctk.set_appearance_mode("light")
+        log("Theme zmeneno:", "Info", self.theme)
+    else:
+        self.theme = "dark"
+        ctk.set_appearance_mode("dark")
+        log("Theme zmeneno:", "Info", self.theme)
+        self.update_idletasks()
+    # konec funkce
 
 
 def run_and_control(self, trida):
+    """funkce na spousteni instanci a kontrola"""
     instance = trida(self)
 
     if isinstance(instance, trida):
@@ -159,17 +189,21 @@ def run_and_control(self, trida):
     else:
         log("Instance fail!!!", "Error", instance)
     return instance
+    # konec funkce
 
 
 def log(msg: str, stav: str, object):
+    """funkce na vypisovani logu"""
     match stav:
         case "Error":
-            logging.error("%s %s", msg, object)
+            lg.error("%s %s", msg, object)
         case "Info":
-            logging.info("%s %s", msg, object)
+            lg.info("%s %s", msg, object)
+    # konec funkce
 
 
 def file_loader():
+    """ziskani jmeno souboru"""
     file = filedialog.askopenfile(
         initialdir="./data",
         title="Select a file",
@@ -183,10 +217,11 @@ def file_loader():
     else:
         log("file name extraction: ", "Error", "FAIL")
         return "No file selected"
+    # konec funkce
 
 
 def file_saver() -> str:
-    """Ahoj"""
+    """ziskani cesty pro ukladani dat"""
 
     file_path: str = filedialog.asksaveasfilename(
         title="Save Data",
@@ -201,6 +236,7 @@ def file_saver() -> str:
     else:
         log("path extraction: ", "Error", "FAIL")
         return "Soubor nebyl vybran"
+    # konec funkce
 
 
 # endregion
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import customtkinter as ctk
-import MoreCustomTkinterWidgets as mckw
+import MoreCustomTkinterWidgets as mctk
 from CTkMenuBar import CTkMenuBar as mb
 from CTkMenuBar import CustomDropdownMenu as cdm
+import logging as lg
 
 import config as conf
 
@@ -9,17 +10,71 @@ import config as conf
 class App(ctk.CTk):
     #
     def __init__(self) -> None:
-        self.theme: str = "system"
+        self.theme: str = "dark"
         self.color: str = "blue"
-        self.mongo_db: bool = False
+        self.mongo_db = False
         #
         super().__init__()
         conf.app_init(self, "mapibase", 1024, 768)
         conf.appearance_mode(self.theme, self.color, ctk)
-        parent, column = conf.create_menu(self, mb, cdm)
-        conf.detect_db(self, ctk, parent, column)
+        parent, column = conf.create_menu(self, mb, cdm, ctk)
+        conf.db_frame(self, ctk, parent, column)
+        conf.detect_db(self, self.mongo_db)
+        #
+        # volame tridy
+        self.main_frame = conf.run_and_control(self, MainFrame)
 
         self.update_idletasks()
+
+
+class MainFrame(ctk.CTkFrame):
+    """Hlavni frame"""
+
+    def __init__(self, parent):
+        self.parent = parent
+        super().__init__(parent, fg_color=parent._fg_color)
+        self.pack(side="top", fill="both", expand=True)
+        #
+        # volame tridy
+        self.header_frame = conf.run_and_control(self, Header)
+        self.left_frame = conf.run_and_control(self, LeftFrame)
+        self.playground = conf.run_and_control(self, PlayGround)
+
+        # MainFrame GRID
+        self.rowconfigure(0, weight=0, uniform="a")
+        self.rowconfigure(1, weight=1, uniform="b")
+        self.columnconfigure(0, weight=0, uniform="a")
+        self.columnconfigure(1, weight=1, uniform="b")
+
+
+class Header(ctk.CTkFrame):
+    """Horni frame"""
+
+    def __init__(self, parent):
+        self.parent = parent
+        super().__init__(
+            parent, height=100, fg_color=parent._fg_color, **conf.layout_config
+        )
+        self.grid(row=0, column=0, columnspan=2, sticky="nsew", padx=2, pady=2)
+        #
+
+
+class LeftFrame(ctk.CTkFrame):
+    """Levy frame"""
+
+    def __init__(self, parent):
+        self.parent = parent
+        super().__init__(parent, fg_color=parent._fg_color, **conf.layout_config)
+        self.grid(row=1, column=0, sticky="nsew", padx=2, pady=2)
+
+
+class PlayGround(ctk.CTkFrame):
+    """Levy frame"""
+
+    def __init__(self, parent):
+        self.parent = parent
+        super().__init__(parent, fg_color=parent._fg_color, **conf.layout_config)
+        self.grid(row=1, column=1, sticky="nsew", padx=2, pady=2)
 
 
 if __name__ == "__main__":
