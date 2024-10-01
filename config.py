082d829f7f3d30a09ecae5737190f22f2e81e64f
Vigo <miroslav.viktorin@seznam.cz>
2024-10-01 12:02:15 +0200

    velky uklid done

--- config.py
+++ config.py
@@ -54,10 +54,11 @@ def img_loader(vstup_dir: str, size_img: int):
 
 def appearance(theme: str, color: str, ctk) -> None:
     # nastaveni Theme a scalingu
+    #
     ctk.set_appearance_mode(theme)
     ctk.set_default_color_theme(color)
     app_theme[0] = theme
-    #
+    # --
     ctk.set_widget_scaling(1.0)
     ctk.set_window_scaling(1.0)
     lg.info("Theme: %s, barva: %s", theme, color)
@@ -66,6 +67,7 @@ def appearance(theme: str, color: str, ctk) -> None:
 
 def app_init(self, name, width, height) -> None:
     """Nastavy zakladni rozmer appky"""
+    #
     self.title(name)
     self.iconbitmap("./assets/ico.ico")
     center_window(self, width, height)
@@ -76,6 +78,7 @@ def app_init(self, name, width, height) -> None:
 
 def center_window(self, app_width: int, app_height: int) -> None:
     """Centers the window on the screen."""
+    #
     self.update_idletasks()
     width: int = app_width
     height: int = app_height
@@ -91,22 +94,23 @@ def center_window(self, app_width: int, app_height: int) -> None:
 # endregion
 #
 # region -- MENU --
-def create_menu(self, mb, cdm,mongo, ctk) -> None:
+def create_menu(self, mb, cdm, mongo, ctk) -> None:
     """Vytvor menu bar"""
-    menu = mb(self, height=40, padx=10, bg_color=("#dbdbdb", "black"))
+    #
+    menu = mb(self, height=40, padx=10, bg_color=("#dbdbdb", "#2b2b2b"))
     buttons: dict = {
         "system_btn": " System ",
         "settings_btn": " Settings ",
         "info_btn": " Info ",
     }
     column: int = len(buttons)  # pocet menu btns
-
+    # --
     config: dict = {
         "corner_radius": border_radius,
         "font": menu_font,
         "hover_color": (light_color, dark_color),
     }
-
+    # --
     for name, label in buttons.items():
         setattr(self, name, menu.add_cascade(label, **config))
     # end buttony -----------------------
@@ -116,7 +120,6 @@ def create_menu(self, mb, cdm,mongo, ctk) -> None:
     self.system_settings = system_drop.add_option(
         "Settings", command=lambda: change_theme(self, ctk)
     )
-
     system_drop.add_separator()
     self.konec = system_drop.add_option("Exit", command=self.destroy)
     # --
@@ -127,9 +130,15 @@ def create_menu(self, mb, cdm,mongo, ctk) -> None:
     self.about = info_drop.add_option("About")
     # --
     # end drop menu -------------------------
+    # --
+    # GRID
     menu.columnconfigure(column, weight=1, uniform="a")
+    # --
+    # volame frame na detekci databaze
     db_frame(self, ctk, menu, column)
-
+    # detekujeme databazi
+    detect_db(self, mongo)
+    # --
     # konec funkce
 
 
@@ -152,6 +161,7 @@ def db_frame(self, ctk, parent, column: int) -> None:
 
 def detect_db(self, mongo) -> None:
     """detekuj databazi"""
+    #
     error_img = img_loader("./assets/error_40.png", 40)
     mongo_img = img_loader("./assets/mongo_40.png", 40)
     match mongo:
@@ -161,7 +171,7 @@ def detect_db(self, mongo) -> None:
                 image=mongo_img,
             )
             log("Mongo: connect", "Info", mongo)
-
+        # --
         case False:
             self.detect_db_label.configure(
                 text="Mongo: disconnected", image=error_img, padx=10
@@ -170,24 +180,29 @@ def detect_db(self, mongo) -> None:
     # konec funkce
 
 
+# endregion
 #
-# region LOGO
+# region -- LOGO --
 def pick_logo():
+    """losovani logo img"""
+    #
     logo_list: list = []
     adresar: str = "./assets/logo/"
-
+    # --
     for soubor in os.listdir(adresar):
         cesta_k_souboru = os.path.join(adresar, soubor)
         logo_list.append(img_loader(cesta_k_souboru, 80))
-
+    # --
     logo = choice(logo_list)
-
+    # --
     return logo
+    # konec funkce
 
 
 # endregion
 #
-# region LeftFrame
+# region -- LeftFrame --
+# left frame buttons
 cfg_frame: dict = {
     "api": [
         "api_frame",
@@ -214,6 +229,7 @@ cfg_frame: dict = {
         img_loader("./assets/geo_40.png", 30),
     ],
 }
+# --
 
 
 def make_frame_label(
@@ -225,47 +241,37 @@ def make_frame_label(
     label_txt: str,
     img,
 ) -> None:
+    """tvorba framu a jeho buttns"""
+    #
     frame = ctk.CTkFrame(self)
     setattr(self, frame_name, frame)
-
+    # --
     label = ctk.CTkLabel(
         frame,
         text=label_txt,
         image=img,
         **left_label_config,
     )
-
     setattr(self, label_name, label)
     label.bind("<Button-1>", button_1)
-
+    # --
     frame.grid(row=frame_row, column=frame_col, sticky="ew", padx=2, pady=1)
-
     label.pack(fill="x", side="top", expand=True)
-
     # konec funkce
 
 
-# endregion
-#
 # endregion
 #
 # region -- FUNKCE --
 #
-def get_family(self):
-    """zjisti vztahy"""
-    temp = self.main_frame.left_frame.winfo_children()
-    labels: list = [item.winfo_children() for item in temp]
-
-    # konec funkce
 
 
 # change theme
 def change_theme(self, ctk) -> None:
-    "zmena theme"
-
+    """zmena theme"""
+    #
     self.theme = "light" if self.theme == "dark" else "dark"
     ctk.set_appearance_mode(self.theme)
-
     log("Theme zmeneno:", "Info", self.theme)
     app_theme[0] = self.theme
     # konec funkce
@@ -273,79 +279,96 @@ def change_theme(self, ctk) -> None:
 
 def run_and_control(self, trida):
     """funkce na spousteni instanci a kontrola"""
+    #
     instance = trida(self)
-
+    # --
     if isinstance(instance, trida):
         log("instance is running.", "Info", instance)
     else:
         log("Instance fail!!!", "Error", instance)
+    # --
     return instance
-    # konec funkce
-
-
-def log(msg: str, stav: str, object):
-    """funkce na vypisovani logu"""
-    temp: tuple = ("%s %s", msg, object)
-    lg.error(*temp) if stav == "Error" else lg.info(*temp)
-
     # konec funkce
 
 
 def file_loader():
     """ziskani jmeno souboru"""
+    #
     file = filedialog.askopenfile(
         initialdir="./data",
         title="Select a file",
         filetypes=[("CSV files", "*.csv"), ("JSON files", "*.json")],
     )
-
+    # --
     if file:
         file_name: str = Path(file.name).name
         log("file name extracted: ", "Info", file_name)
+        # --
         return file
     else:
         log("file name extraction: ", "Error", "FAIL")
+        # --
         return "No file selected"
     # konec funkce
 
 
 def file_saver() -> str:
     """ziskani cesty pro ukladani dat"""
-
+    #
     file_path: str = filedialog.asksaveasfilename(
         title="Save Data",
         initialdir="./data",
         defaultextension=".csv",
         filetypes=[("CSV files", "*.csv"), ("JSON files", "*.json")],
     )
+    # --
     if file_path:
         file: list = list(file_path.split("/"))
         log("path extracted: ", "Info", file[-1])
+        # --
         return file_path
     else:
         log("path extraction: ", "Error", "FAIL")
+        # --
         return "Soubor nebyl vybran"
     # konec funkce
 
 
 def button_1(event) -> None:
     """clicknuti mysi"""
+    #
     widget = event.widget.master  # label
     widget_text = widget.cget("text")
-
     grand_parent = widget.master.master  # cely LeftFrame
-    #
+    # --
     for frame in grand_parent.winfo_children():
         for label in frame.winfo_children():
             label.configure(fg_color="transparent")
-
+    # --
     widget.configure(fg_color=(light_color, "#4f4f4f"))
-
     # konec funkce
 
 
 # endregion
+#
+# region -- SERVISNI FUNKCE --
+def log(msg: str, stav: str, object):
+    """funkce na vypisovani logu"""
+    # servisni funkce
+    temp: tuple = ("%s %s", msg, object)
+    lg.error(*temp) if stav == "Error" else lg.info(*temp)
+    # konec funkce
 
 
+def get_family(self):
+    """zjisti vztahy"""
+    # servisni funkce
+    temp = self.main_frame.left_frame.winfo_children()
+    labels: list = [item.winfo_children() for item in temp]
+    # konec funkce
+
+
+# endregion
+#
 if __name__ == "__main__":
     pass
